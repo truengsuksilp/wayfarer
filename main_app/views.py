8f92a09030080c4b00b3aed921bf836f10bf2dbe
truengsuksilp <truengsuksilp@gmail.com>
2021-11-01 16:35:14 -0700

    Added Home URL and base and home html

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
+# TODO Add Models
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = 'home.html'
