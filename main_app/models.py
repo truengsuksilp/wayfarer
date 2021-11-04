fdd5a00575216fc86d4e33be1869dd40b10e1d0f
alexwill44 <AlArvanitis@gmail.com>
2021-11-04 16:14:18 -0700

    finalized all three models and they are ready to be migrated

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 # import models from django
 
-# model imports from django 
+from django.contrib.auth.models import User
 
+# model imports from django 
 from django.db.models import Model, CharField, ForeignKey, TextField, DateTimeField
 # Create your models here.
-
-""" class Profile(Model):
+class Profile(Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     join_date = DateTimeField(auto_now_add=True)
     name = CharField(max_length=100)
@@ -17,12 +17,12 @@ class City (Model):
     name = CharField(max_length=250)
     country = CharField(max_length=250)
     picture = CharField(max_length = 1000)
-    user = ForeignKey(Profile, on_delete=models.CASCADE,related_name="cities")
+    user = ForeignKey(User, on_delete=models.CASCADE,related_name="cities")
 
 class Post (Model): 
     title = CharField(max_length = 500)
     content = TextField(max_length=10000)
-    user = ForeignKey(Profile, on_delete=models.CASCADE, related_name="posts")
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     city = ForeignKey(City, on_delete=models.CASCADE, related_name="posts")
     created_at = DateTimeField(auto_now_add=True)
 
@@ -33,5 +33,5 @@ class Post (Model):
 
 
   
- """
+
 
