eb84cf1e8826a36758ef52b04aeca79e6ebb677d
Iovannysm <oenjad@gmail.com>
2021-11-04 15:19:23 -0700

    This part was created by the team to get a base

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+# import models from django
+from django.db.models import Model, CharField, DateTimeField, ForeignKey 
+
+import time 
+
 # Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
