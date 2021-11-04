4897f4e0c3b2bba70bf35f3d2b946e8974c5789c
Iovannysm <oenjad@gmail.com>
2021-11-04 15:40:09 -0700

    Resolved issues with mergin

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 # import models from django
-from django.db.models import Model, CharField, DateTimeField, ForeignKey 
-
-import time 
-
 # Create your models here.
 
 
