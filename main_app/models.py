ca99758f471ae559bbcf3b1c86783b8a17607ef3
alexwill44 <AlArvanitis@gmail.com>
2021-11-04 15:18:20 -0700

    created city and post models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+# model imports from django 
+
+from django.db.models import Model, CharField, ForeignKey, TextField
 # Create your models here.
+
+
+
+class City (Model):
+    name = CharField(max_length=250)
+    country = CharField(max_length=250)
+    picture = CharField(max_length = 1000)
+    user = ForeignKey(User, on_delete=models.CASCADE,related_name="cities")
+
+
+
+class Post (Model): 
+    title = CharField(max_length = 500)
+    content = TextField(max_length=10000)
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    city = ForeignKey(City, on_delete=models.CASCADE, related_name="posts")
+    created_at = DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        ordering = ['created_at']
+
+
+  
