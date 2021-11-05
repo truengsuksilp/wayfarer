41e0aaaa26442581d8abc7a24eafaf167844fc97
alexwill44 <AlArvanitis@gmail.com>
2021-11-05 12:36:39 -0700

    update form and page are working + require minor style tweaks

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home, Profile
+from .views import Home, ProfileDetail, ProfileUpdate
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('profile/<int:pk>/', Profile.as_view(), name='profile_detail')
+    path('profile/<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
+    path('profile/<int:pk>/update/', ProfileUpdate.as_view(), name='profile_update')
 ]
