bab6460ca8cf4fe06ebff96c7329526f3151d405
Iovannysm <oenjad@gmail.com>
2021-11-05 15:21:31 -0700

    Fixed login and logout as well as signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+
 
 
 
@@ -10,8 +12,8 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('profile/<int:pk>/', ProfileDetail.as_view(), name='profile'),
     path('accounts/signup/', views.Signup.as_view(), name='signup'),
-    path('accounts/login/', views.Login.as_view(), name='login' ),
     path('profile/<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
     path('profile/<int:pk>/update/', ProfileUpdate.as_view(), name='profile_update'),
-    path('profile/<int:profile_pk>/posts/<int:pk>/', Post.as_view(), name='post_detail')
+    path('profile/<int:profile_pk>/posts/<int:pk>/', Post.as_view(), name='post_detail'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
