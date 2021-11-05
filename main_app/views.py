cf8b1390f6fe3f2812305266aff0a577455a83d2
Iovannysm <oenjad@gmail.com>
2021-11-05 10:28:30 -0700

    Added the routes and views fo login and signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -6,6 +6,12 @@ from django.urls import reverse
 from main_app.models import Profile, Post, City
 # TODO Add Models
 
+# import the class that will handle basic views
+from django.views import View
+
+# auth imports
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 # Create your views here.
 
 class Home(TemplateView):
@@ -20,3 +26,29 @@ class Profile(DetailView):
         context["post"] = Post.objects.filter(user = Profile)
         return context
 
+
+# Signup view
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, "registration/signup.html", context)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+            return redirect("profile")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
+
+#Login View 
+class Login(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        context={"form": form}
+        return render(request, "registration/login.html", context)
