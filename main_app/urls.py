cd0f13cca7f15a2bcb2e3122ac514a8e61fef9b8
alexwill44 <AlArvanitis@gmail.com>
2021-11-05 14:29:53 -0700

    merged

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from .views import Home, Profile, Post, ProfileDetail, ProfileUpdate
+from .views import Home, Post, ProfileDetail, ProfileUpdate
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('profile/<int:pk>/', Profile.as_view(), name='profile'),
     path('accounts/signup/', views.Signup.as_view(), name='signup'),
     path('accounts/login/', views.Login.as_view(), name='login' ),
     path('profile/<int:pk>/', ProfileDetail.as_view(), name='profile_detail'),
