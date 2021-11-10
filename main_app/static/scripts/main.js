5ea153a633c631ded9d4a8befc8818ed5ed1d0ec
truengsuksilp <truengsuksilp@gmail.com>
2021-11-09 16:21:29 -0800

    City Page: Added custom forms

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,2 +1,18 @@
 console.log('=== I am in ===');
 
+const textTooLong = function textTooLong(targetElement, appendToElement, text_length){
+    if ( targetElement.val().length > text_length ){
+        appendToElement.append(`<p class='error'>Text is more than ${text_length} characters, which is a little long<p>`);
+    }
+}
+
+// Event listener to parent applies to all child
+// $('p.Post.Content').on("click", (event) => titleTooLong());
+
+$('#id_title').on('change', (event) => 
+    textTooLong($('#id_title'),$('p.Post.Title'), 50)
+)
+
+$('#id_content').on('change', (event) => 
+    textTooLong($('#id_content'),$('p.Post.Content'), 200)
+)
